04083e083b79fff54d8b1b52597df6ab67ff1690
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2021-11-27 01:34:06 -0800

    Added comments to code

--- NoTankYou/TerritoryManager.cs
+++ NoTankYou/TerritoryManager.cs
@@ -20,21 +20,30 @@ namespace NoTankYou
         }
         private void InitalizeAllianceRaidTerritoryTypeList()
         {
+            // Get database of territory types
             var territorySheets = Service.DataManager.GetExcelSheet<TerritoryType>();
 
+            // check that we actually got a table,
+            // because visual studio seems to think theres a high chance we might not have gotten one
             if (territorySheets != null)
             {
+                // Select the rows that contain a IntendedUse of 8
                 // IntendedUse value of 8 is Alliance Raid
                 AllianceRaidTerritoryTypes = territorySheets.Where(r => r.TerritoryIntendedUse == 8).Select(r => r.RowId).ToArray();
             }
         }
+
+        // Triggers on Map Change
         private void OnTerritoryChanged(object? sender, ushort e)
         {
+            // Debug data
             Service.Chat.Print($"[NoTankYou] Territory Changed. NewID: {e}");
 
+            // Determine current state
             bool disabledBecauseAllianceRaid = IsAllianceRaid() && Service.Configuration.DisableInAllianceRaid;
             bool disabledBecauseBlacklist = Service.Configuration.TerritoryBlacklist.Contains(e);
 
+            // Either enable or disable the banner depending on state
             if (disabledBecauseAllianceRaid || disabledBecauseBlacklist)
             {
                 warningWindow.Active = false;
@@ -44,9 +53,14 @@ namespace NoTankYou
                 warningWindow.Active = true;
             }
 
+            // Delay rendering for InstanceLoadDelayTime milliseconds
+            // Territory Change and party formation happens BEFORE the loading screen appears
+            // We must delay rendering or itll show awkwardly during loading screen, also gives the tank a "grace period"
             warningWindow.Delayed = true;
             Task.Delay(Service.Configuration.InstanceLoadDelayTime).ContinueWith(t => { warningWindow.Delayed = false; });
         }
+
+        // Only checks the currently occupied territory for match in alliance raid database
         public bool IsAllianceRaid()
         {
             var currentTerritory = Service.ClientState.TerritoryType;
