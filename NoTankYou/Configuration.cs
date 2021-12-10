247b8ee9b801ee821e87b917f43ddea8f64db953
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2021-12-09 18:35:13 -0800

    Fixes Default Configuration Value on update.

--- NoTankYou/Configuration.cs
+++ NoTankYou/Configuration.cs
@@ -71,6 +71,13 @@ namespace NoTankYou
         public void Initialize(DalamudPluginInterface pluginInterface)
         {
             this.pluginInterface = pluginInterface;
+
+            if( Version == 1)
+            {
+                ShowMoveWarningBanner = false;
+                Version = 2;
+                Save();
+            }
         }
 
         public void Save()
