b33d953719a9e7d95967567259a8175a29e96d8f
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2021-12-16 19:46:30 -0800

    Faerie Banner, Split logic into more manageable functions

--- NoTankYou/DisplaySystem/Banners/FaerieBanner.cs
+++ NoTankYou/DisplaySystem/Banners/FaerieBanner.cs
@@ -5,6 +5,7 @@ using System;
 using System.Linq;
 using System.Numerics;
 using Dalamud.Game.ClientState.Conditions;
+using Dalamud.Game.ClientState.Buddy;
 
 namespace NoTankYou.DisplaySystem
 {
@@ -51,62 +52,72 @@ namespace NoTankYou.DisplaySystem
             // If we are in a party, and in a duty
             if (Service.PartyList.Length > 0 && Service.Condition[ConditionFlag.BoundByDuty])
             {
-                // Scholar Job id is 28
-                var scholarPlayers = Service.PartyList.Where(p => p.ClassJob.Id is 28);
-
-                var scholarPlayerIDs = scholarPlayers.Select(r => r.ObjectId);
-
-                // Get the objects that have owner ids matching those of our scholars
-                var objectsWithPartyMemberOwner = Service.ObjectTable
-                    .Where(r => scholarPlayerIDs.Contains(r.OwnerId));
-
-                // id 791 is dissipation id
-                var dissipationEffects = scholarPlayers
-                    .Where(r => r.Statuses.Any(s => s.StatusId is 791));
-
-
-                // If these two lists match, then everyone's doing their job
-                if (scholarPlayers.Count() == objectsWithPartyMemberOwner.Count() + dissipationEffects.Count())
-                {
-                    Visible = false;
-                }
-
-                // If not, then we need to show the warning banner
-                else
-                {
-                    Visible = true;
-                }
+                UpdateInPartyInDuty();
             }
 
             // If we are in a duty, and have solo mode enabled
             else if (Service.Configuration.EnableFaerieBannerWhileSolo && Service.Condition[ConditionFlag.BoundByDuty])
             {
-                var player = Service.ClientState.LocalPlayer;
-
-                if (player == null) return;
-
-                // find any pet that has the player as an owner
-                var objectWithPlayerOwnerExists = Service.ObjectTable
-                    .Any(r => r.OwnerId == player.ObjectId);
-
-                // id 791 is dissipation id
-                // Check if the player has dissipation
-                bool playerHasDissipation = player.StatusList.Any(s => s.StatusId is 791);
-
-                // If player has dissipation, or a faerie out, they are doing their job
-                if (playerHasDissipation || objectWithPlayerOwnerExists)
-                {
-                    Visible = false;
-                }
-                // If not, then we need to show the warning banner
-                else
-                {
-                    Visible = true;
-                }
+                UpdateSoloInDuty();
             }
+
             else
             {
                 Visible = false;
+            }
+        }
+
+        private void UpdateInPartyInDuty()
+        {
+            // Scholar Job id is 28
+            var scholarPlayers = Service.PartyList.Where(p => p.ClassJob.Id is 28);
+
+            var scholarPlayerIDs = scholarPlayers.Select(r => r.ObjectId);
+
+            // Get the objects that have owner ids matching those of our scholars
+            var objectsWithPartyMemberOwner = Service.ObjectTable
+                .Where(r => scholarPlayerIDs.Contains(r.OwnerId));
+
+            // id 791 is dissipation id
+            var dissipationEffects = scholarPlayers
+                .Where(r => r.Statuses.Any(s => s.StatusId is 791));
+
+            // If these two lists match, then everyone's doing their job
+            if (scholarPlayers.Count() == objectsWithPartyMemberOwner.Count() + dissipationEffects.Count())
+            {
+                Visible = false;
+            }
+
+            // If not, then we need to show the warning banner
+            else
+            {
+                Visible = true;
+            }
+        }
+
+        private void UpdateSoloInDuty()
+        {
+            var player = Service.ClientState.LocalPlayer;
+
+            if (player == null) return;
+
+            // find any pet that has the player as an owner
+            var objectWithPlayerOwnerExists = Service.ObjectTable
+                .Any(r => r.OwnerId == player.ObjectId);
+
+            // id 791 is dissipation id
+            // Check if the player has dissipation
+            bool playerHasDissipation = player.StatusList.Any(s => s.StatusId is 791);
+
+            // If player has dissipation, or a faerie out, they are doing their job
+            if (playerHasDissipation || objectWithPlayerOwnerExists)
+            {
+                Visible = false;
+            }
+            // If not, then we need to show the warning banner
+            else
+            {
+                Visible = true;
             }
         }
 
