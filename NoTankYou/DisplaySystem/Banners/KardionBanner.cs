bbed1cb369f6a9c6dcdfb7d49c9eecc7b431595c
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2021-12-19 15:33:24 -0800

    Cleaned up Kardion code

--- NoTankYou/DisplaySystem/Banners/KardionBanner.cs
+++ NoTankYou/DisplaySystem/Banners/KardionBanner.cs
@@ -15,48 +15,21 @@ namespace NoTankYou.DisplaySystem.Banners
 
         protected override void UpdateInPartyInDuty()
         {
-            // ClassJob 40 is Sage, get the list of all Sages in the party
-            var partyList = Service.PartyList.Where(r => r.ClassJob.Id is 40);
-
-            // Kardion Status is 2605, get the list of party members with Kardion
-            // Kardia Status is 2604 (This goes on the sage)
-            var membersWithKardion = Service.PartyList.Where(r => r.Statuses.Any(s => s.StatusId == 2604));
-
-            // If these two lists match, then everyone's doing their job
-            if (partyList.Count() == membersWithKardion.Count())
-            {
-                Visible = false;
-            }
-
-            // If not, then we need to show the warning banner
-            else
-            {
-                Visible = true;
-            }
+            Visible = !Service.PartyList
+                .Where(r => r.ClassJob.Id is 40)
+                .Any(p => p.Statuses.Any(s => s.StatusId is 2604));
         }
 
         protected override void UpdateSoloInDuty()
         {
             var player = Service.ClientState.LocalPlayer;
-
             if (player == null) return;
 
-            // If the player isn't a sage, then this is irrelevent
-            if (player.ClassJob.Id != 40) return;
+            bool playerIsSage = player.ClassJob.Id == 40;
 
-            // Does player have Kardia
             var playerHasKardia = player.StatusList.Any(s => s.StatusId is 2604);
 
-            // If the player has Kardia on themselves, they are doing their job
-            if (playerHasKardia)
-            {
-                Visible = false;
-            }
-            // If not, then we need to show the warning banner
-            else
-            {
-                Visible = true;
-            }
+            Visible = playerIsSage && !playerHasKardia;
         }
     }
 }
