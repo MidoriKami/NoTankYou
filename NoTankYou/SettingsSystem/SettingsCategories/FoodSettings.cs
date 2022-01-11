c9197f9e39319ee586d0fe92694bf5b3d47b0658
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-01-11 04:00:36 -0800

    Added Search Dropdowns

--- NoTankYou/SettingsSystem/SettingsCategories/FoodSettings.cs
+++ NoTankYou/SettingsSystem/SettingsCategories/FoodSettings.cs
@@ -5,18 +5,34 @@ using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using Dalamud.Interface.Components;
+using Dalamud.Logging;
 using ImGuiNET;
+using Lumina.Excel.GeneratedSheets;
 
 namespace NoTankYou.SettingsSystem.SettingsCategories
 {
     internal class FoodSettings : BannerSettings
     {
+        private readonly List<ContentFinderCondition> ContentFinderConditionList;
+        private readonly HashSet<string> ContentTypeList;
+        private HashSet<string> InstanceNames;
+        private string SelectedContentTypeString = "";
+        private string SelectedInstanceName = "";
+
         private int ModifyWhitelistValue;
 
         public FoodSettings()
         {
             CategoryName = "Food Warning Settings";
             TabName = "Food";
+
+            ContentFinderConditionList = Service.DataManager.GetExcelSheet<ContentFinderCondition>()!.ToList();
+
+            ContentTypeList = ContentFinderConditionList
+                !.Where(c => c.ContentType.Value?.Name != null)
+                .Select(c => c.ContentType.Value!.Name.ToString())
+                .ToHashSet();
+
         }
 
         protected override ref Configuration.ModuleSettings Settings => ref Service.Configuration.FoodSettings;
@@ -34,6 +50,12 @@ namespace NoTankYou.SettingsSystem.SettingsCategories
             PrintAddRemoveCurrentTerritoryWhitelist();
 
             PrintAddRemoveManualTerritoryWhitelist();
+
+            PrintAddRemoveManualWithNameTerritoryWhitelist();
+
+            ImGui.Text("Food Specific Settings");
+            ImGui.Separator();
+            ImGui.Spacing();
 
             DrawEarlyWarningTime();
 
@@ -62,6 +84,7 @@ namespace NoTankYou.SettingsSystem.SettingsCategories
             ImGui.Spacing();
 
         }
+
         private static void PrintAddRemoveCurrentTerritoryWhitelist()
         {
             ImGui.Text("Whitelist Operations");
@@ -99,6 +122,7 @@ namespace NoTankYou.SettingsSystem.SettingsCategories
 
             ImGui.Spacing();
         }
+
         private void PrintAddRemoveManualTerritoryWhitelist()
         {
             ImGui.Text("Manually Add or Remove");
@@ -128,6 +152,106 @@ namespace NoTankYou.SettingsSystem.SettingsCategories
 
             ImGui.Spacing();
         }
+
+        private void PrintAddRemoveManualWithNameTerritoryWhitelist()
+        {
+
+            ImGui.Text("Add or Remove by DutyFinder Name");
+            ImGui.Separator();
+            ImGui.Spacing();
+
+            if (ImGui.BeginCombo("##ContentTypeSelection", SelectedContentTypeString))
+            {
+                foreach (var name in ContentTypeList)
+                {
+                    bool isSelected = name == SelectedContentTypeString;
+                    if (ImGui.Selectable(name, isSelected))
+                    {
+                        SelectedContentTypeString = name;
+                        InstanceNames = ContentFinderConditionList
+                            .Where(c => c.ContentType.Value != null)
+                            .Where(c => c.Name != null)
+                            .Where(c =>  c.ContentType.Value!.Name == SelectedContentTypeString)
+                            .Select(c => c.Name.ToString())
+                            .ToHashSet();
+                    }
+
+                    if (isSelected)
+                    {
+                        ImGui.SetItemDefaultFocus();
+                    }
+                }
+
+                ImGui.EndCombo();
+            }
+            ImGui.Spacing();
+
+            if (SelectedContentTypeString != "")
+            {
+                if (ImGui.BeginCombo("##TerritorySelectByName", SelectedInstanceName))
+                {
+
+                    foreach (var instanceName in InstanceNames)
+                    {
+                        bool isSelected = instanceName == SelectedInstanceName;
+                        if (ImGui.Selectable(instanceName, isSelected))
+                        {
+                            SelectedInstanceName = instanceName;
+                        }
+
+                        if (isSelected)
+                        {
+                            ImGui.SetItemDefaultFocus();
+                        }
+                    }
+
+                    ImGui.EndCombo();
+                }
+
+                ImGui.Spacing();
+
+
+                if (ImGui.Button("Add Instance", new(125, 25)))
+                {
+                    var whitelist = Service.Configuration.FoodTerritoryWhitelist;
+
+                    var instanceId = (int)ContentFinderConditionList
+                        .Where(c => c.Name != null)
+                        .First(c => c.Name.ToString() == SelectedInstanceName)
+                        .TerritoryType.Value!.RowId;
+
+                    if (!whitelist.Contains(instanceId))
+                    {
+
+                            
+                        whitelist.Add(instanceId);
+                        Service.Configuration.ForceWindowUpdate = true;
+                    }
+                }
+
+                ImGui.SameLine();
+
+                if (ImGui.Button("Remove Instance", new(125, 25)))
+                {
+                    var whitelist = Service.Configuration.FoodTerritoryWhitelist;
+
+                    var instanceId = (int)ContentFinderConditionList
+                        .Where(c => c.Name != null)
+                        .First(c => c.Name.ToString() == SelectedInstanceName)
+                        .TerritoryType.Value!.RowId;
+
+                    if (whitelist.Contains(instanceId))
+                    {
+                        whitelist.Remove(instanceId);
+                        Service.Configuration.ForceWindowUpdate = true;
+                    }
+                }
+            }
+
+            ImGui.Spacing();
+
+        }
+
         private static void RemoveFromWhitelist(int territory)
         {
             var blacklist = Service.Configuration.FoodTerritoryWhitelist;
@@ -154,7 +278,7 @@ namespace NoTankYou.SettingsSystem.SettingsCategories
             ImGui.InputFloat("Early Warning Time (Seconds)", ref Service.Configuration.FoodEarlyWarningTime, 0, 0);
             ImGui.PopItemWidth();
             ImGuiComponents.HelpMarker("How many seconds before food expires to show a warning for.\n" +
-                                       "Minimim: 0\n" +
+                                       "Minimum: 0\n" +
                                        "Maximum: 5400");
 
             if (Service.Configuration.FoodEarlyWarningTime < 0)
