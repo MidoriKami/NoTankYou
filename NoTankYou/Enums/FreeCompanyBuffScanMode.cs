5b6dc4c4eccad53aae53a544a01368aa26a40721
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-05-20 20:31:43 -0700

    Directives Cleanup

--- NoTankYou/Enums/FreeCompanyBuffScanMode.cs
+++ NoTankYou/Enums/FreeCompanyBuffScanMode.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NoTankYou.Enums
+﻿namespace NoTankYou.Enums
 {
     public enum FreeCompanyBuffScanMode
     {
--- NoTankYou/Modules/FreeCompanyModule.cs
+++ NoTankYou/Modules/FreeCompanyModule.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Dalamud.Game.ClientState.Objects.SubKinds;
 using Lumina.Excel.GeneratedSheets;
 using NoTankYou.Data.Components;
@@ -10,7 +7,6 @@ using NoTankYou.Data.Modules;
 using NoTankYou.Enums;
 using NoTankYou.Interfaces;
 using NoTankYou.Localization;
-using NoTankYou.Utilities;
 
 namespace NoTankYou.Modules
 {
@@ -51,7 +47,6 @@ namespace NoTankYou.Modules
                     {
                         var targetStatus = Service.DataManager.GetExcelSheet<Status>()!.GetRow(Settings.BuffList[i])!.RowId;
 
-                        // If this status is missing, send warning
                         if (!character.StatusList.Any(status => status.StatusId == targetStatus))
                         {
                             return true;
