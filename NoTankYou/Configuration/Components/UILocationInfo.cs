d0a9ffdb048cffffeb8ac1d6f6c4b00e71c140e5
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-09-06 11:49:17 -0700

    Remove Unused UILocation class

--- NoTankYou/Configuration/Components/UILocationInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Numerics;
-
-namespace NoTankYou.Configuration.Components;
-
-public struct UILocationInfo
-{
-    public Vector2 Position { get; set; }
-    public Vector2 Size { get; set; }
-}
