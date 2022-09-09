cca464926179fc00f2d5e1059f1aa09123536634
Gabriel Mejia <griver08@gmail.com>
2022-09-08 21:55:31 -0400

    SpiritBond Module

--- NoTankYou/System/ModuleManager.cs
+++ NoTankYou/System/ModuleManager.cs
@@ -17,6 +17,7 @@ internal class ModuleManager
         new Sage(),
         new Scholar(),
         new Summoner(),
+        new SpiritBond(),
     };
 
     public IEnumerable<ISelectable> GetConfigurationSelectables()
