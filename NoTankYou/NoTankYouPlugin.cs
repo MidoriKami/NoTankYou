57c4033be578cc9ffe54d818e871a59b29c6409a
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-29 01:33:34 -0800

    Update KamiLib

--- NoTankYou/NoTankYouPlugin.cs
+++ NoTankYou/NoTankYouPlugin.cs
@@ -1,5 +1,5 @@
 ﻿using Dalamud.Plugin;
-using KamiLib.Utilities;
+using KamiLib;
 using NoTankYou.Overlays;
 using NoTankYou.System;
 using NoTankYou.Windows;
@@ -16,36 +16,32 @@ public sealed class NoTankYouPlugin : IDalamudPlugin
         // Create Static Services for use everywhere
         pluginInterface.Create<Service>();
 
-        KamiLib.KamiLib.Initialize(pluginInterface, Name, () => Service.ConfigurationManager.Save());
+        KamiCommon.Initialize(pluginInterface, Name, () => Service.ConfigurationManager.Save());
         
         // Systems that have no dependencies
         Service.FontManager = new FontManager();
-        Service.LocalizationManager = new LocalizationManager();
         Service.PartyListAddon = new PartyListAddon();
-        Service.DutyEventManager = new DutyEventManager();
-        
+
         // Dependent systems below
         Service.ConfigurationManager = new ConfigurationManager();
         Service.ModuleManager = new ModuleManager();
         
-        KamiLib.KamiLib.CommandManager.AddHandler(ShorthandCommand, "shorthand command to open configuration window");
+        KamiCommon.CommandManager.AddHandler(ShorthandCommand, "shorthand command to open configuration window");
         
-        KamiLib.KamiLib.WindowManager.AddWindow(new ConfigurationWindow());
-        KamiLib.KamiLib.WindowManager.AddWindow(new PartyListOverlayWindow());
-        KamiLib.KamiLib.WindowManager.AddWindow(new BannerOverlayWindow());
-        KamiLib.KamiLib.WindowManager.AddWindow(new PartyOverlayConfigurationWindow());
-        KamiLib.KamiLib.WindowManager.AddWindow(new BannerOverlayConfigurationWindow());
-        KamiLib.KamiLib.WindowManager.AddWindow(new BlacklistConfigurationWindow());
+        KamiCommon.WindowManager.AddWindow(new ConfigurationWindow());
+        KamiCommon.WindowManager.AddWindow(new PartyListOverlayWindow());
+        KamiCommon.WindowManager.AddWindow(new BannerOverlayWindow());
+        KamiCommon.WindowManager.AddWindow(new PartyOverlayConfigurationWindow());
+        KamiCommon.WindowManager.AddWindow(new BannerOverlayConfigurationWindow());
+        KamiCommon.WindowManager.AddWindow(new BlacklistConfigurationWindow());
     }
         
     public void Dispose()
     {
-        KamiLib.KamiLib.Dispose();
+        KamiCommon.Dispose();
         
         Service.FontManager.Dispose();
-        Service.LocalizationManager.Dispose();
         Service.PartyListAddon.Dispose();
-        Service.DutyEventManager.Dispose();
 
         Service.ConfigurationManager.Dispose();
     }
--- NoTankYou/Service.cs
+++ NoTankYou/Service.cs
@@ -1,33 +1,24 @@
-﻿using Dalamud.Data;
-using Dalamud.Game;
+﻿using Dalamud.Game;
 using Dalamud.Game.ClientState;
 using Dalamud.Game.ClientState.Objects;
 using Dalamud.Game.ClientState.Party;
 using Dalamud.Game.Gui;
-using Dalamud.Interface.Windowing;
 using Dalamud.IoC;
 using Dalamud.Plugin;
-using Condition = Dalamud.Game.ClientState.Conditions.Condition;
 
 namespace NoTankYou;
 
 public class Service
 {
     [PluginService] public static DalamudPluginInterface PluginInterface { get; private set; } = null!;
-    [PluginService] public static ClientState ClientState { get; private set; } = null!;
-    [PluginService] public static PartyList PartyList { get; private set; } = null!;
-    [PluginService] public static Condition Condition { get; private set; } = null!;
-    [PluginService] public static DataManager DataManager { get; private set; } = null!;
-    [PluginService] public static Framework Framework { get; private set; } = null!;
-    [PluginService] public static ObjectTable ObjectTable { get; private set; } = null!;
+    [PluginService] public static ClientState ClientState { get; private set;} = null!;
+    [PluginService] public static PartyList PartyList { get; private set;} = null!;
+    [PluginService] public static Framework Framework { get; private set;} = null!;
+    [PluginService] public static ObjectTable ObjectTable { get; private set;} = null!;
     [PluginService] public static GameGui GameGui { get; private set; } = null!;
 
-    public static WindowSystem WindowSystem { get; set; } = new("NoTankYou");
-
     internal static System.ModuleManager ModuleManager = null!;
-    internal static System.LocalizationManager LocalizationManager = null!;
     internal static System.ConfigurationManager ConfigurationManager = null!;
     internal static System.FontManager FontManager = null!;
-    internal static System.DutyEventManager DutyEventManager = null!;
     internal static System.PartyListAddon PartyListAddon = null!;
 }
