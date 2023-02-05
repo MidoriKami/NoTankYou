2ea1885d6f1dab4abcb93436b21f2f9a2803a8f4
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-02-04 22:42:24 -0800

    Suppress warnings for dead until 3 seconds after revive.

--- NoTankYou/Interfaces/ILogicComponent.cs
+++ NoTankYou/Interfaces/ILogicComponent.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using Dalamud.Game.ClientState.Objects.SubKinds;
 using KamiLib.GameState;
 using NoTankYou.DataModels;
@@ -10,15 +11,22 @@ public interface ILogicComponent
     IModule ParentModule { get; }
     List<uint> ClassJobs { get; }
 
-    WarningState? EvaluateWarning(PlayerCharacter character);
+    private static readonly Dictionary<uint, Stopwatch> DeathStopwatch = new();
 
+    WarningState? EvaluateWarning(PlayerCharacter character);
+    
     WarningState? ShouldShowWarning(PlayerCharacter character)
     {
         if (!ParentModule.GenericSettings.Enabled) return null;
         if (ParentModule.GenericSettings.DutiesOnly && !DutyState.Instance.IsDutyStarted) return null;
         if (ParentModule.GenericSettings.SoloMode && character.ObjectId != Service.ClientState.LocalPlayer?.ObjectId) return null;
-        if (character.IsDead) return null;
 
+        if (character.IsDead) DeathStopwatch[character.ObjectId] = Stopwatch.StartNew();
+        if (DeathStopwatch.TryGetValue(character.ObjectId, out var lastDeath) && lastDeath.Elapsed.TotalSeconds < 3)
+        {
+            return null;
+        }
+        
         return EvaluateWarning(character);
     }
 }
