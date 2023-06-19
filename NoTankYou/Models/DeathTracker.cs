8e046cb22f19132b0af46cb058e6687df450042b
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-06-18 23:34:30 -0700

    DeathTracker.cs
    Increase Grace Period from 3 seconds to 5 seconds

--- NoTankYou/Models/DeathTracker.cs
+++ NoTankYou/Models/DeathTracker.cs
@@ -17,10 +17,10 @@ public class DeathTracker
         // If they are in the dictionary then they have recently died, or been dead, or have recently been revived
         if (deathStopwatch.TryGetValue(playerData.GetObjectId(), out var lastDeath))
         {
-            // if its been less than 3 seconds since death, being dead, or revived, return they are dead
-            if (lastDeath.Elapsed < TimeSpan.FromSeconds(3)) return true;
+            // if its been less than 5 seconds since death, being dead, or revived, return they are dead
+            if (lastDeath.Elapsed < TimeSpan.FromSeconds(5)) return true;
             
-            // if its been more than 3 seconds, then they are definitely alive, remove from dictionary
+            // if its been more than 5 seconds, then they are definitely alive, remove from dictionary
             deathStopwatch.Remove(playerData.GetObjectId());
         }
         
