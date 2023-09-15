9a899be6844b799342737cc92e20b929bf6d3a12
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-09-15 15:38:28 -0700

    Clean up HasPet logic

--- NoTankYou/Models/Interfaces/IPlayerData.cs
+++ NoTankYou/Models/Interfaces/IPlayerData.cs
@@ -39,8 +39,7 @@ public unsafe interface IPlayerData
     bool HasPet()
         => Service.ObjectTable
             .Where(obj => obj.OwnerId == GetObjectId())
-            .Where(obj => obj.ObjectKind is ObjectKind.BattleNpc)
-            .Where(obj => (obj as BattleNpc)?.SubKind == (byte) BattleNpcSubKind.Pet)
+            .Where(obj => obj is BattleNpc { SubKind: (byte) BattleNpcSubKind.Pet })
             .Any();
     
     // Internal Helpers
