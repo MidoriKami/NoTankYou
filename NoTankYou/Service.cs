150ef6a37fee986dc7acfa2817a683449d5db4a8
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-03-10 11:28:50 -0700

    Revert "Use AddonLifecycle for NativeUI Modifications"
    This reverts commit a8bd5cfafd1a7c2173133e7d20b52d4e858892c1.

--- NoTankYou/Service.cs
+++ NoTankYou/Service.cs
@@ -15,5 +15,4 @@ public class Service
     [PluginService] public static IPluginLog Log { get; private set; } = null!;
     [PluginService] public static ITextureProvider TextureProvider { get; private set; } = null!;
     [PluginService] public static ICondition Condition { get; private set; } = null!;
-    [PluginService] public static IAddonLifecycle AddonLifecycle { get; private set; } = null!;
 }
