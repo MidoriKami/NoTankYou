fd8ed1c03fea1ddad56e660c0a6c9e53dc9655c9
MidoriKami <mi_kami@outlook.com>
2024-06-18 13:41:49 -0700

    Add Disable While Solo option to Sage

--- NoTankYou/Modules/Sage.cs
+++ NoTankYou/Modules/Sage.cs
@@ -1,4 +1,6 @@
-﻿using NoTankYou.Classes;
+﻿using FFXIVClientStructs.FFXIV.Client.Game.Group;
+using ImGuiNET;
+using NoTankYou.Classes;
 using NoTankYou.Localization;
 using NoTankYou.PlayerDataInterface;
 
@@ -13,7 +15,8 @@ public class Sage : ModuleBase<SageConfiguration> {
     private const int KardiaStatusId = 2604;
     private const int KardiaActionId = 24285;
     
-    protected override bool ShouldEvaluate(IPlayerData playerData) {
+    protected override unsafe bool ShouldEvaluate(IPlayerData playerData) {
+        if (Config.DisableWhileSolo && GroupManager.Instance()->MemberCount is 0) return false;
         if (playerData.MissingClassJob(SageClassJob)) return false;
         if (playerData.GetLevel() < MinimumLevel) return false;
 
@@ -27,4 +30,10 @@ public class Sage : ModuleBase<SageConfiguration> {
     }
 }
 
-public class SageConfiguration() : ModuleConfigBase(ModuleName.Sage);
+public class SageConfiguration() : ModuleConfigBase(ModuleName.Sage) {
+    public bool DisableWhileSolo = true;
+    
+    protected override void DrawModuleConfig() {
+        ConfigChanged = ImGui.Checkbox("Disable while Solo", ref DisableWhileSolo);
+    }
+};
