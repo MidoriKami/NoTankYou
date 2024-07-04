7d092eb18e8bacd310c6b77ff5fc9eb91024420f
MidoriKami <mi_kami@outlook.com>
2024-07-03 19:25:35 -0700

    Reimplement ParytyListController

--- NoTankYou/Controllers/PartyListController.cs
+++ NoTankYou/Controllers/PartyListController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Numerics;
@@ -8,56 +9,58 @@ using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using Dalamud.Interface;
 using Dalamud.Interface.Utility;
 using Dalamud.Interface.Utility.Raii;
+using Dalamud.Utility;
+using Dalamud.Utility.Numerics;
+using FFXIVClientStructs.FFXIV.Client.Graphics;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 using FFXIVClientStructs.Interop;
 using ImGuiNET;
-using KamiLib.Components;
+using KamiLib.Classes;
 using KamiLib.Configuration;
 using KamiLib.Extensions;
+using KamiToolKit.Classes;
+using KamiToolKit.Nodes;
 using NoTankYou.Classes;
 using NoTankYou.Localization;
 
 namespace NoTankYou.Controllers;
 
 public unsafe class PartyListController : IDisposable {
-    public PartyListConfig Config { get; private set; } = new();
+    private static readonly Stopwatch AnimationStopwatch = Stopwatch.StartNew();
+    private static bool AnimationState => !System.PartyListController.Config.Animation || AnimationStopwatch.ElapsedMilliseconds < System.PartyListController.Config.AnimationPeriod / 2.0f;
 
-    private PartyListMemberOverlay?[] partyMembers = new PartyListMemberOverlay[8];
+    private PartyListConfig Config { get; set; } = new();
 
-    public PartyListController() {
-        var partyListAddon = (AddonPartyList*) Service.GameGui.GetAddonByName("_PartyList");
-        if (partyListAddon is not null) {
-            AttachToNative(partyListAddon);
-        }
-        
-        Service.AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "_PartyList", OnPartyListSetup);
-        Service.AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, "_PartyList", OnPartyListFinalize);
-    }
+    private readonly IconImageNode[] jobIconWarningNodes = new IconImageNode[8];
+    private readonly ImageNode[] warningTypeNodes = new ImageNode[8];
+    private ByteColor originalOutlineColor;
+    private bool isAttached;
+
+    private static AddonPartyList* AddonPartyList => (AddonPartyList*) Service.GameGui.GetAddonByName("_PartyList");
 
     public void Dispose() {
         Unload();
-        
-        Service.AddonLifecycle.UnregisterListener(OnPartyListSetup);
-        Service.AddonLifecycle.UnregisterListener(OnPartyListFinalize);
     }
     
     public void Load() {
         Config = PartyListConfig.Load();
+        
+        Service.AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "_PartyList", OnPartyListSetup);
+        Service.AddonLifecycle.RegisterListener(AddonEvent.PreFinalize, "_PartyList", OnPartyListFinalize);
+        
+        if (AddonPartyList is not null) {
+            AttachToNative(AddonPartyList);
+        }
     }
 
     public void Unload() {
-        foreach (var member in partyMembers) {
-            member?.Reset();
-        }
+        Service.AddonLifecycle.UnregisterListener(OnPartyListSetup);
+        Service.AddonLifecycle.UnregisterListener(OnPartyListFinalize);
         
-        var partyListAddon = (AddonPartyList*) Service.GameGui.GetAddonByName("_PartyList");
-        if (partyListAddon is not null) {
-            DetachFromNative(partyListAddon);
-        }
-        
-        foreach (var member in partyMembers) {
-            member?.Dispose();
+        if (AddonPartyList is not null) {
+            DetachFromNative(AddonPartyList);
         }
     }
 
@@ -65,20 +68,19 @@ public unsafe class PartyListController : IDisposable {
         => Config.DrawConfigUi();
     
     public void Update() {
-        foreach (var member in partyMembers) {
-            member?.Update();
+        if (AnimationStopwatch.ElapsedMilliseconds > System.PartyListController.Config.AnimationPeriod) {
+            AnimationStopwatch.Restart();
         }
     }
 
     public void Draw(List<WarningState> warnings) {
         if (!Config.Enabled) return;
+        if (!isAttached) return;
 
-        foreach (var partyMember in partyMembers) {
-            partyMember?.Reset();
-        }
+        ResetPartyMembers();
         
         if (Config.SampleMode) {
-            partyMembers[0]?.DrawWarning(ModuleController.SampleWarning);
+            DrawWarning(0, ModuleController.SampleWarning);
             return;
         }
         
@@ -88,40 +90,73 @@ public unsafe class PartyListController : IDisposable {
                 .Where(warning => warning.SourceEntityId == Service.ClientState.LocalPlayer?.EntityId)
                 .MaxBy(warning => warning.Priority);
             
-            partyMembers[0]?.DrawWarning(warning);
+            DrawWarning(0, warning);
         }
         else {
-            foreach (var index in Enumerable.Range(0, partyMembers.Length)) {
-                var partyMember = partyMembers[index];
-                var hudPartyMember = AgentHUD.Instance()->PartyMembers[index];
+            foreach(var index in Enumerable.Range(0, 8)) {
+                var hudPartyMember = AgentHUD.Instance()->PartyMembers.GetPointer(index);
                 
                 var warning = warnings
                     .Where(warning => !Config.BlacklistedModules.Contains(warning.SourceModule))
-                    .Where(warning => warning.SourceEntityId == hudPartyMember.EntityId)
+                    .Where(warning => warning.SourceEntityId == hudPartyMember->EntityId)
                     .MaxBy(warning => warning.Priority);
                 
-                partyMember?.DrawWarning(warning);
+                DrawWarning(index, warning);
             }
         }
     }
-    
+
     private void OnPartyListSetup(AddonEvent type, AddonArgs args) {
         AttachToNative((AddonPartyList*)args.Addon);
     }
     
     private void AttachToNative(AddonPartyList* addonPartyList) {
         Service.Framework.RunOnFrameworkThread(() => {
-            partyMembers = new PartyListMemberOverlay[8];
-
-            foreach (var index in Enumerable.Range(0, 8)) {
-                var partyMemberData = addonPartyList->PartyMembers.GetPointer(index);
-
-                var partyMemberOverlay = new PartyListMemberOverlay(partyMemberData, addonPartyList);
-                
-                partyMemberOverlay.EnableTooltip(addonPartyList);
-                
-                partyMembers[index] = partyMemberOverlay;
+            if (isAttached) {
+                Service.Log.Info("Attempted to Attach to PartyList after already attached.");
+                return;
             }
+            
+            originalOutlineColor = addonPartyList->Pet.Name->EdgeColor;
+            
+            foreach (var index in Enumerable.Range(0, 8)) {
+                ref var partyMember = ref AddonPartyList->PartyMembers[index];
+
+                warningTypeNodes[index] = new ImageNode {
+                    NodeID = 20000 + partyMember.PartyMemberComponent->OwnerNode->NodeId,
+                    NodeFlags = NodeFlags.Visible,
+                    Size = new Vector2(24.0f, 24.0f),
+                    X = partyMember.ClassJobIcon->GetXFloat() + 16.0f,
+                    Y = partyMember.ClassJobIcon->GetYFloat(),
+                    IsVisible = false,
+                };
+                
+                System.NativeController.AttachToComponent(
+                    warningTypeNodes[index],
+                    (AtkUnitBase*)addonPartyList, 
+                    partyMember.PartyMemberComponent, 
+                    (AtkResNode*)partyMember.ClassJobIcon, 
+                    NodePosition.AfterTarget);
+                
+                jobIconWarningNodes[index] = new IconImageNode {
+		            NodeID = 10000 + partyMember.PartyMemberComponent->OwnerNode->NodeId,
+		            NodeFlags = NodeFlags.Visible,
+		            Size = new Vector2(32.0f, 32.0f),
+		            X = partyMember.ClassJobIcon->GetXFloat(),
+		            Y = partyMember.ClassJobIcon->GetYFloat(),
+		            IsVisible = false,
+                    IconId = 60074,
+                };
+                
+                System.NativeController.AttachToComponent(
+                    jobIconWarningNodes[index],
+                    (AtkUnitBase*)addonPartyList, 
+                    partyMember.PartyMemberComponent, 
+                    (AtkResNode*)partyMember.ClassJobIcon, 
+                    NodePosition.AfterTarget);
+            }
+
+            isAttached = true;
         }); 
     }
     
@@ -131,15 +166,96 @@ public unsafe class PartyListController : IDisposable {
     
     private void DetachFromNative(AddonPartyList* addonPartyList) {
         Service.Framework.RunOnFrameworkThread(() => {
-            foreach (var index in Enumerable.Range(0, 8)) {
-                var partyMemberData = addonPartyList->PartyMembers.GetPointer(index);
-                ref var overlayData = ref partyMembers[index];
-                
-                overlayData?.Detach(partyMemberData->PartyMemberComponent, addonPartyList);
-                overlayData?.Dispose();
-                overlayData = null;
+            if (!isAttached) {
+                Service.Log.Info("Attempted to Detach from PartyList without being attached.");
+                return;
             }
+
+            foreach (var index in Enumerable.Range(0, 8)) {
+                ref var partyMember = ref AddonPartyList->PartyMembers[index];
+                                
+                System.NativeController.DetachFromComponent(warningTypeNodes[index], (AtkUnitBase*)addonPartyList, partyMember.PartyMemberComponent);
+                warningTypeNodes[index].Dispose();
+                
+                System.NativeController.DetachFromComponent(jobIconWarningNodes[index], (AtkUnitBase*)addonPartyList, partyMember.PartyMemberComponent);
+                jobIconWarningNodes[index].Dispose();
+            }
+            
+            ResetPartyMembers();
+
+            isAttached = false;
         });
+    }
+    
+    private void ResetPartyMembers() {
+	    if (AddonPartyList is null) return;
+	    
+        foreach (var index in Enumerable.Range(0, 8)) {
+            ref var memberComponent = ref AddonPartyList->PartyMembers[index];
+
+            memberComponent.Name->EdgeColor = originalOutlineColor;
+            memberComponent.ClassJobIcon->ToggleVisibility(true);
+        }
+    }
+
+    private void DrawWarning(int index, WarningState? warning) {
+	    if (AddonPartyList is null) return;
+        if (warning is null) return;
+		
+		if (System.PartyListController.Config.JobIcon) {
+			AnimateWarningIcon(index, warning);
+		}
+
+		if (System.PartyListController.Config.PlayerName) {
+			AnimePlayerNameColor(index);
+		}
+    }
+    
+    private void AnimateWarningIcon(int index, WarningState warning) {
+	    if (AddonPartyList is null) return;
+        ref var memberComponent = ref AddonPartyList->PartyMembers[index];
+
+	    if (AnimationState) {
+            jobIconWarningNodes[index].IsVisible = true;
+            memberComponent.ClassJobIcon->ToggleVisibility(false);
+            warningTypeNodes[index].IsVisible = true;
+            SetWarningTypeTexture(warningTypeNodes[index], warning);
+	    }
+	    else {
+            jobIconWarningNodes[index].IsVisible = false;
+            memberComponent.ClassJobIcon->ToggleVisibility(true);
+            warningTypeNodes[index].IsVisible = false;
+        }
+    }
+
+    private void AnimePlayerNameColor(int index) {
+	    if (AddonPartyList is null) return;
+        ref var memberComponent = ref AddonPartyList->PartyMembers[index];
+
+	    if (AnimationState) {
+            memberComponent.Name->EdgeColor = Config.OutlineColor.ToByteColor();
+	    }
+	    else {
+            memberComponent.Name->EdgeColor = originalOutlineColor;
+	    }
+	}
+
+    private void SetWarningTypeTexture(ImageNode imageNode, WarningState warningState) {
+        if (warningState.SourceModule is ModuleName.Tanks) {
+            if (imageNode.LoadedIconId is not uint.MaxValue) {
+                imageNode.LoadTexture("ui/uld/fourth/LFG_hr1.tex");
+                imageNode.TextureCoordinates = new Vector2(0.0f, 216.0f);
+                imageNode.ImageNodeFlags = 0;
+                imageNode.Size = new Vector2(56.0f, 56.0f);
+                imageNode.Scale = new Vector2(24.0f / 56.0f);  
+            }
+        }
+        else {
+            var warningIcon = warningState.SourceModule.GetAttribute<ModuleIconAttribute>()!.SimpleIcon;
+            if (imageNode.LoadedIconId != warningIcon) {
+                imageNode.LoadIcon(warningIcon);
+            }
+        }
     }
 }
 
