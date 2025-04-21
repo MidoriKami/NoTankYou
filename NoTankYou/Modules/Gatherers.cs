2f6b834bfca93cb0f890d9889fd2621145328690
MidoriKami <mi_kami@outlook.com>
2025-04-20 23:19:51 -0700

    Style update

--- NoTankYou/Modules/Gatherers.cs
+++ NoTankYou/Modules/Gatherers.cs
@@ -13,31 +13,33 @@ public class Gatherers : ModuleBase<GatherersConfig> {
     private const uint MinerClassJobId = 16;
     private const uint BotanistClassJobId = 17;
     private const uint FisherClassJobId = 18;
+
+    private record GathererJobData(uint ClassJob, uint MinLevel, uint StatusId, uint ActionId);
     
-    private readonly List<(uint ClassJob, uint MinLevel, uint StatusId, uint ActionId)> data = [
-        (MinerClassJobId, 1, 225, 227),
-        (MinerClassJobId, 46, 222, 238),
-        (BotanistClassJobId, 1, 217, 210),
-        (BotanistClassJobId, 46, 221, 221),
-        (FisherClassJobId, 61, 1166, 7903),
-        (FisherClassJobId, 65, 1173, 7911),
-        (FisherClassJobId, 50, 805, 4101),
+    private readonly List<GathererJobData> data = [
+        new (MinerClassJobId, 1, 225, 227),
+        new (MinerClassJobId, 46, 222, 238),
+        new (BotanistClassJobId, 1, 217, 210),
+        new (BotanistClassJobId, 46, 221, 221),
+        new (FisherClassJobId, 61, 1166, 7903),
+        new (FisherClassJobId, 65, 1173, 7911),
+        new (FisherClassJobId, 50, 805, 4101),
     ];
     
     protected override bool ShouldEvaluate(IPlayerData playerData) 
         => playerData.GetClassJob() is 16 or 17 or 18;
 
     protected override void EvaluateWarnings(IPlayerData playerData) {
-        foreach (var (classJob, minLevel, statusId, actionId) in data) {
-            if (!Config.Miner && classJob is MinerClassJobId) continue;
-            if (!Config.Botanist && classJob is BotanistClassJobId) continue;
-            if (!Config.Fisher && classJob is FisherClassJobId) continue;
+        foreach (var jobData in data) {
+            if (!Config.Miner && jobData.ClassJob is MinerClassJobId) continue;
+            if (!Config.Botanist && jobData.ClassJob is BotanistClassJobId) continue;
+            if (!Config.Fisher && jobData.ClassJob is FisherClassJobId) continue;
 
             // Collectors Glove
-            if (statusId is 805 && (!Config.CollectorsGlove || !playerData.HasClassJob(FisherClassJobId))) continue;
+            if (jobData.StatusId is 805 && (!Config.CollectorsGlove || !playerData.HasClassJob(FisherClassJobId))) continue;
 
-            if (playerData.GetLevel() >= minLevel && playerData.MissingStatus(statusId)) {
-                AddActiveWarning(actionId, playerData);
+            if (playerData.GetLevel() >= jobData.MinLevel && playerData.MissingStatus(jobData.StatusId)) {
+                AddActiveWarning(jobData.ActionId, playerData);
             }
         }
     }
