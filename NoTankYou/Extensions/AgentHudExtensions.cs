bc825ed8fddb300734733cc8435bc6c5fc42a7e9
MidoriKami <mi_kami@outlook.com>
2025-07-18 15:07:32 -0700

    Add AgentHUD extensions

---
+++ NoTankYou/Extensions/AgentHudExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+
+namespace NoTankYou.Extensions;
+
+public static class AgentHudExtensions {
+	public static HudPartyMember? GetMember(ref this AgentHUD agentHud, ulong entityId) {
+		foreach (var member in agentHud.PartyMembers) {
+			if (member.EntityId == entityId) {
+				return member;
+			}
+		}
+
+		return null;
+	}
+}
