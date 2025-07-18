ed428cefe18b2c472a18be6751c41358d75cb3e6
MidoriKami <mi_kami@outlook.com>
2025-07-18 15:07:55 -0700

    Remove unused events
    Remove nameplate checks

--- NoTankYou/Classes/ModuleBase.cs
+++ NoTankYou/Classes/ModuleBase.cs
@@ -3,21 +3,15 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Numerics;
-using System.Text.Json.Serialization;
 using Dalamud.Game.ClientState.Conditions;
 using Dalamud.Interface;
-using Dalamud.Interface.Utility;
-using Dalamud.Interface.Utility.Raii;
 using Dalamud.Utility;
 using FFXIVClientStructs.FFXIV.Client.Game.Character;
 using FFXIVClientStructs.FFXIV.Client.Game.Group;
 using FFXIVClientStructs.FFXIV.Client.Game.UI;
-using FFXIVClientStructs.FFXIV.Component.GUI;
 using FFXIVClientStructs.Interop;
 using ImGuiNET;
-using KamiLib.Classes;
 using KamiLib.CommandManager;
-using KamiLib.Configuration;
 using KamiLib.Extensions;
 using NoTankYou.Localization;
 using NoTankYou.PlayerDataInterface;
@@ -28,10 +22,8 @@ namespace NoTankYou.Classes;
 public abstract class ModuleBase {
     public abstract ModuleName ModuleName { get; }
     public abstract void Load();
-    public abstract void Unload();
     public abstract void DrawConfigUi();
     public abstract void EvaluateWarnings();
-    public virtual void ZoneChange(uint newZoneId) { }
     public List<WarningState> ActiveWarningStates { get; } = [];
     public bool HasWarnings => ActiveWarningStates.Count != 0;
     
@@ -51,11 +43,8 @@ public abstract unsafe class ModuleBase<T> : ModuleBase, IDisposable where T : M
     
     private readonly Dictionary<ulong, Stopwatch> suppressionTimer = new();
     
-    private static AtkUnitBase* NameplateAddon 
-        => (AtkUnitBase*)Service.GameGui.GetAddonByName("NamePlate");
-    
     private readonly DeathTracker deathTracker = new();
-    
+
     public virtual void Dispose() { }
     
     private string ModuleCommand => ModuleName.ToString();
@@ -82,9 +71,6 @@ public abstract unsafe class ModuleBase<T> : ModuleBase, IDisposable where T : M
         ActiveWarningStates.Clear();
 
         if (!Config.Enabled) return;
-        if (NameplateAddon is null) return;
-        if (!NameplateAddon->IsReady) return;
-        if (!NameplateAddon->IsVisible) return;
         if (Service.ClientState.IsPvPExcludingDen) return;
         if (System.BlacklistController.IsZoneBlacklisted(Service.ClientState.TerritoryType)) return;
         if (System.SystemConfig.WaitUntilDutyStart && Service.Condition.IsBoundByDuty() && !Service.DutyState.IsDutyStarted) return;
@@ -165,13 +151,6 @@ public abstract unsafe class ModuleBase<T> : ModuleBase, IDisposable where T : M
         Config = ModuleConfigBase.Load<T>(ModuleName);
     }
 
-    public override void Unload() {
-        Service.Log.Debug($"[{ModuleName}] Unloading Module");
-    }
-    
-    public void ZoneChange(ushort _)
-        => suppressedObjectIds.Clear();
-
     protected static Span<PartyMember> PartyMemberSpan 
         => GroupManager.Instance()->MainGroup.PartyMembers[..GroupManager.Instance()->MainGroup.MemberCount];
 
@@ -230,98 +209,4 @@ public abstract unsafe class ModuleBase<T> : ModuleBase, IDisposable where T : M
 
     private void PrintConfirmation() 
         => Service.Chat.Print(Strings.Command, Config.Enabled ? $"{Strings.Enabling} {ModuleName.GetDescription()}" : $"{Strings.Disabling} {ModuleName.GetDescription()}");
-}
-
-[Flags]
-public enum OptionDisableFlags {
-    None = 0,
-    SoloMode = 1 << 1,
-    DutiesOnly = 1 << 2,
-    Sanctuary = 1 << 3,
-}
-
-public abstract class ModuleConfigBase(ModuleName moduleName) {
-    public bool Enabled;
-    public bool SoloMode;
-    public bool DutiesOnly = true;
-    public bool DisableInSanctuary = true;
-    public int Priority;
-    public bool CustomWarning;
-    public string CustomWarningText = string.Empty;
-    public bool AutoSuppress;
-    public int AutoSuppressTime = 60;
-
-    [JsonIgnore] protected bool ConfigChanged { get; set; }
-    [JsonIgnore] public virtual OptionDisableFlags OptionDisableFlags => OptionDisableFlags.None;
-
-    public virtual void DrawConfigUi() {
-        ImGuiTweaks.Header(Strings.General);
-        using (var _ = ImRaii.PushIndent()) {
-            ConfigChanged |= ImGui.Checkbox(Strings.Enable, ref Enabled);
-            
-            using (var disable = ImRaii.Disabled(OptionDisableFlags.HasFlag(OptionDisableFlags.SoloMode))) {
-                ConfigChanged |= ImGuiTweaks.Checkbox(Strings.SoloMode, ref SoloMode, Strings.SoloModeHelp);
-                DisabledSettingTooltip(disable);
-            }
-            
-            using (var disable = ImRaii.Disabled(OptionDisableFlags.HasFlag(OptionDisableFlags.DutiesOnly))) {
-                ConfigChanged |= ImGuiTweaks.Checkbox(Strings.DutiesOnly, ref DutiesOnly, Strings.DutiesOnlyHelp);
-                DisabledSettingTooltip(disable);
-            } 
-            
-            using (var disable = ImRaii.Disabled(OptionDisableFlags.HasFlag(OptionDisableFlags.Sanctuary))) {
-                ConfigChanged |= ImGuiTweaks.Checkbox(Strings.HideInSanctuary, ref DisableInSanctuary, Strings.HideInSanctuaryHelp);
-                DisabledSettingTooltip(disable);
-            } 
-            
-            ConfigChanged |= ImGuiTweaks.PriorityInt(Service.PluginInterface, Strings.Priority, ref Priority);
-        }
-
-        if (!OptionDisableFlags.HasFlag(OptionDisableFlags.SoloMode)) {
-            ImGuiTweaks.Header("Warning Suppression");
-            using (ImRaii.PushIndent()) {
-                ConfigChanged |= ImGuiTweaks.Checkbox("Auto Suppress", ref AutoSuppress, $"Automatically suppress warnings for other players after {AutoSuppressTime} Seconds");
-
-                ImGui.PushItemWidth(50.0f * ImGuiHelpers.GlobalScale);
-                ConfigChanged |= ImGui.InputInt("Suppression Time (Seconds)", ref AutoSuppressTime, 0, 0);
-            }
-        }
-
-        ImGuiTweaks.Header(Strings.DisplayOptions);
-        using (var _ = ImRaii.PushIndent()) {
-            ConfigChanged |= ImGui.Checkbox(Strings.CustomWarning, ref CustomWarning);
-            
-            ImGui.SetNextItemWidth(ImGui.GetContentRegionAvail().X);
-            ConfigChanged |= ImGui.InputTextWithHint("##Custom_Warning_Text", Strings.WarningText, ref CustomWarningText, 1024);
-        } 
-        
-        ImGuiTweaks.Header(Strings.ModuleOptions);
-        using (var _ = ImRaii.PushIndent()) {
-            ImGuiHelpers.ScaledDummy(5.0f);
-            DrawModuleConfig();
-        }
-
-        if (ConfigChanged) {
-            Service.Log.Verbose($"Saving config for {moduleName}");
-            Save();
-            ConfigChanged = false;
-        }
-    }
-
-    protected void DisabledSettingTooltip(ImRaii.IEndObject endObject) {
-        if (endObject.Success && ImGui.IsItemHovered(ImGuiHoveredFlags.AllowWhenDisabled)) {
-            using var fullAlpha = ImRaii.PushStyle(ImGuiStyleVar.Alpha, 1.0f);
-            ImGui.SetTooltip("This setting is being ignored");
-        }
-    }
-
-    protected virtual void DrawModuleConfig() {
-        ImGui.TextColored(KnownColor.Orange.Vector(), "No additional options for this module");
-    }
-	
-    public static T Load<T>(ModuleName moduleName) where T : new()
-        => Service.PluginInterface.LoadCharacterFile(Service.ClientState.LocalContentId, $"{moduleName}.config.json", () => new T());
-	
-    public void Save()
-        => Service.PluginInterface.SaveCharacterFile(Service.ClientState.LocalContentId, $"{moduleName}.config.json", this);
 }
